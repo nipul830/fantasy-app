7347e3c48f279e92631a3d26aa4f86aa85d24665
nipul830 <57082785+nipul830@users.noreply.github.com>
2025-12-26 19:27:18 +0530

    Update main.dart

--- android_app/main.dart
+++ android_app/main.dart
@@ -1,1 +1,18 @@
 
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fantasy App',
+      home: LoginPage(),
+    );
+  }
+}
