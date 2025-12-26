513da9e31036ad78ba60057294fad180782f86e3
nipul830 <57082785+nipul830@users.noreply.github.com>
2025-12-26 19:28:29 +0530

    Update register_page.dart

--- android_app/register_page.dart
+++ android_app/register_page.dart
@@ -1,1 +1,55 @@
+import 'package:flutter/material.dart';
 
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Register")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 15),
+
+            TextField(
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                labelText: "Mobile Number",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 15),
+
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 25),
+
+            SizedBox(
+              width: double.infinity,
+              height: 45,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("REGISTER"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
